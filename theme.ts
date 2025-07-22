734c555cb56306e8cac21379b2d91252c6d033a4
fernando-paredes <ferparmur@gmail.com>
2025-07-23 00:05:50 +0100

    feat: add theme file

---
+++ theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    black900: "#000",
+    black800: "#171717",
+    black700: "#333333",
+    white100: "#FFFFFF",
+    white200: "#FAFAFA",
+    white300: "#EDEDED",
+    gray500: "#7C7C7C",
+    punch500: "#DA421E",
+    red500: "#C60000",
+    red600: "#9D0100",
+    yellow500: "#EFD61F",
+    watercourse500: "#057C5E",
+  },
+};
